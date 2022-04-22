f075f03f880b314a3b3e0d892b5ef0c21493fe20
a.alves1337@gmail.com <a.alves1337@gmail.com>
2022-04-22 11:53:46 +0200

    modif photo en cours

--- controller/command.php
+++ controller/command.php
@@ -4,20 +4,27 @@ require '../model/db.class.php';
 require '../model/pizza.class.php';
 require '../view/index.class.php'; 
 require '../view/cartes.class.php'; 
+require '../view/photo.class.php';
 
 
 $url = filter_input(INPUT_GET, "url"); // on récupère ce qu'il y a dans l'url saisie par l'utilisateur
 
 switch($url) {
+    case "index.html" :
+    case "" :
+        $page = new Accueil;
+        $titre = "Pizzeria de la plage - Accueil";
+    break;
+
     case "carte.html" :
         $pizzaList = Pizza::list();
         $page = new Carte($pizzaList);
         $titre = "Pizzeria de la plage - Carte";
-        break;
+    break;
 
-    case "index.html" :
-        $page = new Accueil;
-        $titre = "Pizzeria de la plage - Accueil";
+    case "photo.html" :
+        $page = new Photo;
+        $titre = "Pizzeria de la plage - Photo";
     break;
 
     default : 
@@ -25,3 +32,18 @@ switch($url) {
         die();
     break;
 }
+
+if (isset($_FILES['photo']['tmp_name'])) {  
+    $taille = getimagesize($_FILES['photo']['tmp_name']); // met la largeur et la longueur dans un array 
+    $largeur = $taille[0];
+    $hauteur = $taille[1];
+    $extension = exif_imagetype($_FILES['photo']['tmp_name']);
+    $largeur_miniature = 300;
+    $hauteur_miniature = $hauteur / $largeur * $largeur_miniature; // Une petite règle de trois pour retailler la largeur de l'image à 300 pixels avec une hauteur proportionnelle
+    $im = imagecreatefromjpeg($_FILES['photo']['tmp_name']); // Stocke toute la photo dans la variable $im
+    $im_miniature = imagecreatetruecolor($largeur_miniature, $hauteur_miniature); // Prépare une image tampon noire en 24 bits d'une largeur de 300 pixels avec une hauteur proportionnelle à la photo d'origine.
+    imagecopyresampled($im_miniature, $im, 0, 0, 0, 0, $largeur_miniature, $hauteur_miniature, $largeur, $hauteur);
+    imagejpeg($im_miniature, './img/upload/miniatures/'.$_FILES['photo']['name'], 90);
+    echo $extension;
+}
+?>
---
+++ view/photo.class.php
@@ -0,0 +1,13 @@
+<?php
+   class Photo {
+      public function html() {
+       echo ' 
+       <h1>Photo</h1>
+       <form action="photo.html" method="POST" enctype="multipart/form-data">
+       <label for="photo">Fichier</label>
+       <input type="file" name="photo">
+       <button type="submit">Enregistrer</button>
+      </form>
+         ';
+      }
+   }
