643acfde7c7f68408bb5d35e6fe21073ad308d84
ludexplo <henry.ludovic@outlook.com>
2022-04-25 16:47:44 +0200

    ModifAffichageCarteFini

--- view/cartes.class.php
+++ view/cartes.class.php
@@ -10,21 +10,37 @@
              <h1>Carte des pizzas</h1>
                 <div class="carte">';
           foreach($this->liste as $pizza) {
+             
              echo '
                  <div class="pizzacarte">
                  <div class="imgpizzacarte"><img src="./img/pizza_'.$pizza->getId().'.jpg" /></div>					
                  <h2>'.$pizza->getNom().'</h2>
                  <p>'.$pizza->getDescription().'</p>
                  <div class="prix">
-                     <div class="tarifs">
-                         <div>La Part = '.number_format($pizza->getPrixPart()/100,2).' € </div><div><input type="number" placeholder="Quantité" min="1" max="8" id="qp1p"/><button class="btn" title="ajoutez au panier"><i class="fas fa-shopping-basket"></i></button></div>
+                 <div class="tarifs">
+                 ';
+                 
+                 if($pizza->getPrixPart() !== -1) {
+                    echo
+                     '   
+                     <div>La Part = '.number_format($pizza->getPrixPart()/100,2).' € </div><div>
+                     <input type="number" placeholder="Quantité" min="1" max="8" id="qp1p"/>
+                     <button class="btn" title="ajoutez au panier"><i class="fas fa-shopping-basket"></i>
+                     </button>
+                     </div>
+                     ';
+                     }
+                     echo
+                     '
+
                          <div>Moyenne = '.number_format($pizza->getPrixPetite()/100,2).' € </div><div><input type="number" placeholder="Quantité" min="1" max="8" id="qp1m"/><button class="btn" title="ajoutez au panier"><i class="fas fa-shopping-basket"></i></button></div>
                          <div>Grande = '.number_format($pizza->getPrixGrande()/100,2).' € </div><div><input type="number" placeholder="Quantité" min="1" max="8" id="qp1g"/><button class="btn" title="ajoutez au panier"><i class="fas fa-shopping-basket"></i></button></div>
                      </div>
                  </div>
              </div>
              ';
-          }
+          
+            };
           echo '
           </div>
           ';
