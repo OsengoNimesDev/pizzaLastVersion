76e6f82da0a826c98e77863307d2d75ae0373329
ScrewUcaptain <artosud.basile.brusseau@gmail.com>
2022-05-02 11:49:25 +0200

    css formulaire de connexion

--- view/formulaire.class.php
+++ view/formulaire.class.php
@@ -19,6 +19,7 @@ class Formulaire
             echo
             '
         <h1>Formulaire de connexion</h1>
+        <div class="formulaire">
         <form action="connexion.html" method="POST">
 
             <label for="email"> Email : </label>
@@ -27,14 +28,46 @@ class Formulaire
             <label for="password">Mot de Passe :</label>
             <input type="password" name="password" /><br>
 
-            <input type="submit" value="Login">
+            <input id="submit" type="submit" value="Login">
 
-        </form>
-        ';
+        </form></div>
+        
+        '; 
         }
     }
 
     public function htmlInscription()
     {
+        echo
+        "
+        <h1>Formulaire d'inscription</h1>
+        <form action='verification.php' method='POST'>
+
+            <label for='mail'>Email : </label>
+            <input type='email' name='mail' placeholder='Entrez votre email' /><br>
+
+            <label for='nom'>Nom : </label>
+            <input type='text' name='nom' placeholder='Entrez votre nom' /><br>
+
+            <label for='prenom'>Prénom : </label>
+            <input type='text' name='prenom' placeholder='Entrez votre prénom' /><br>
+
+            <label for='adresse'>Adresse : </label>
+            <input type='text' name='adresse' placeholder='Entrez votre adresse' /><br>
+
+            <label for='tel'>Téléphone : </label>
+            <input type='text' name='tel' placeholder='Entrez votre numéro de téléphone' /><br>
+
+            <label for='password'>Mot de Passe : </label>
+            <input type='text' name='password' placeholder='Entrez votre mot de passe' /><br>
+
+            <label for='password'>Mot de Passe : </label>
+            <input type='text' name='password' placeholder='Confirmez votre mot de passe' /><br>
+
+            <button type='submit' value='S'inscrire'></button>
+
+        </form>
+        ";
+
     }
 }
