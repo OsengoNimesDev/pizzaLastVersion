e8b5545a5c362d18e20852beaa45c1c473588499
loicpaut <loicpaut@hotmail.fr>
2022-05-05 08:31:06 +0200

    modification du à une erreur de mon coté

--- view/formulaire.class.php
+++ view/formulaire.class.php
@@ -13,7 +13,6 @@ class Formulaire {
             <input type="email" name="email" placeholder="Entrez votre email" /><br>
             <label for="password">Mot de Passe :</label>
             <input type="password" name="password" /><br>
-<<<<<<< HEAD
 
             <input type="submit" value="Login">
 
@@ -22,42 +21,4 @@ class Formulaire {
         }
     }
 
-    public function htmlInscription()
-    {
-        echo
-        '
-        <h1>Formulaire d\'inscription</h1>
-        <form action="verification.php" method="POST">
 
-            <label for="mail">Email : </label>
-            <input type="email" name="mail" placeholder="Entrez votre email" /><br>
-
-            <label for="nom">Nom : </label>
-            <input type="text" name="nom" placeholder="Entrez votre nom" /><br>
-
-            <label for="prenom">Prénom : </label>
-            <input type="text" name="prenom" placeholder="Entrez votre prénom" /><br>
-
-            <label for="adresse">Adresse : </label>
-            <input type="text" name="adresse" placeholder="Entrez votre adresse" /><br>
-
-            <label for="tel">Téléphone : </label>
-            <input type="text" name="tel" placeholder="Entrez votre numéro de téléphone" /><br>
-
-            <label for="password">Mot de Passe : </label>
-            <input type="text" name="password" placeholder="Entrez votre mot de passe" /><br>
-
-            <label for="password">Mot de Passe : </label>
-            <input type="text" name="password" placeholder="Confirmez votre mot de passe" /><br>
-
-            <button type="submit" value="S\'inscrire"></button>
-
-        </form>
-        ';
-=======
-            <input id="submit" type="submit" value="Login">
-        </form></div>
-        '; 
->>>>>>> main
-    }
-}
