a5832001a0a8b550d798cb923ba6e76d45927ceb
jpmannini@gmail.com <jean-pierre@mannini.fr>
2022-05-11 11:45:05 +0200

    On s'occupe du panier

--- view/panier.class.php
+++ view/panier.class.php
@@ -5,6 +5,16 @@ class Panier{
     }
     public function html(){
         print_r($_SESSION);
-    
+        //print_r($_SESSION['panier']);
+        $panier=[];
+        if(isset($_SESSION['panier'])) {
+            foreach($_SESSION['panier'] as $cle => $valeur) {
+                $tabcom=explode("_", $cle);
+                print_r($tabcom);
+            }
+           // $panier=explode("_", $_SESSION['panier']);
+            
+        }
+        //print_r($panier);
     }
 }
