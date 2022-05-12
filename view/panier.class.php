3aa1e8003d46bb345dfecfc070214c28c716ff4b
a.alves1337@gmail.com <a.alves1337@gmail.com>
2022-05-12 14:37:34 +0200

    ajout somme du panier

--- view/panier.class.php
+++ view/panier.class.php
@@ -3,6 +3,7 @@ class Panier{
     public function html(){
         // print_r($_SESSION);
         $panier=[];
+        $somme=0;
         if (isset($_SESSION['panier'])) {
             echo '<div class="listCommand">';
             if (isset($_SESSION['ref_cli'])) {echo ' <h1>Le panier de '.$_SESSION["prenom"]." ".$_SESSION["nom"].'</h1>';}
@@ -36,14 +37,22 @@ class Panier{
                                 break;
                         }
 
-                        $prixPizza=number_format(($prixUnitaire*$valeur)/100,2);
+                        $prixPizza=$prixUnitaire*$valeur;
+                        $prixPizzaFormatee=number_format($prixPizza/100,2);
+                        $somme += $prixPizza;
 
                         echo "
                             <div>$nomPizza</div>
                             <div>$valeur $taillePizza</div>
-                            <div>$prixPizza €</div>
+                            <div>$prixPizzaFormatee €</div>
                             ";
                 }
+
+                        $sommeFormatee=number_format($somme/100,2);
+                echo "
+                            <div class='totalPanier '>Montant total :</div>
+                            <div>$sommeFormatee €</div>
+                            ";
                 echo '</div>';
                 if (isset($_SESSION['ref_cli'])) {echo '<br><input type="submit" value="Valider la commande">';}
                 else {echo '<br>Vous devez vous inscrire pour valider la commande : <input type="submit" value="S\'inscrire">';}
