13024b3139c946a25f82bbd02ea731e7416e0681
ludexplo <henry.ludovic@outlook.com>
2022-05-12 15:45:19 +0200

    var_dump($_SESSION) en commentaire

--- controller/command.php
+++ controller/command.php
@@ -19,7 +19,7 @@ require '../view/commande.class.php';
 $url = filter_input(INPUT_GET, "url"); // on récupère ce qu'il y a dans l'url saisie par l'utilisateur
 
 
-var_dump($_SESSION);
+// var_dump($_SESSION);
 
 switch($url) {
     case "index.html" :
