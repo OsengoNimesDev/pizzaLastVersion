5654ba5044375263f4e391c293e73fd9f354409f
a.alves1337@gmail.com <a.alves1337@gmail.com>
2022-05-12 12:00:40 +0200

    Formatage du panier

--- view/panier.class.php
+++ view/panier.class.php
@@ -4,34 +4,52 @@ class Panier{
         // print_r($_SESSION);
         $panier=[];
         if (isset($_SESSION['panier'])) {
+            echo '<div class="listCommand">';
+            if (isset($_SESSION['ref_cli'])) {echo ' <h1>Le panier de '.$_SESSION["prenom"]." ".$_SESSION["nom"].'</h1>';}
+            else {echo ' <h1>Votre panier</h1>';}
+            echo '
+            <div>
+               <div>Nom de la Pizza</div>
+               <div>Quantité</div>
+               <div>Prix</div>';
                 foreach ($_SESSION['panier'] as $cle => $valeur) {
                     $tabcom=explode("_", $cle);
                     // print_r($tabcom);
-                    $pizza =Pizza::getById($tabcom[1]);
-                    echo $pizza->getNom();
+                    $pizza = Pizza::getById($tabcom[1]);
+                    $nomPizza = $pizza->getNom();
                     $taille=$tabcom[2];
 
                         switch($taille){
                              case "p":
-                                echo " part ";
+                                $taillePizza = "part";
                                  $prixUnitaire = $pizza->getPrixPart();
                                 break;
 
                              case "m":
-                                    echo " moyen ";
+                                $taillePizza = "petite";
                                     $prixUnitaire = $pizza->getPrixPetite();
 
                                     break;
 
                              case "g":
-                                        echo " grande ";
+                                $taillePizza = "grande";
                                         $prixUnitaire = $pizza-> getPrixGrande();
                                         break;
 
                         }
-                        echo $prixUnitaire." quantité ".$valeur."<br>";
-                }
 
+                        $prixPizza=number_format(($prixUnitaire*$valeur)/100,2);
+
+                        echo "
+                            <div>$nomPizza</div>
+                            <div>$valeur $taillePizza</div>
+                            <div>$prixPizza €</div>
+                            ";
+                }
+                echo '</div>';
+                if (isset($_SESSION['ref_cli'])) {echo ' <input type="submit" value="Valider la commande">';}
+                else {echo '<br>Vous devez vous inscrire pour valider la commande : <input type="submit" value="S\'inscrire">';}
+                echo '</div>';
         }
     }
 }
